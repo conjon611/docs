8f20377d2b365bdccda6dbaf422b8533f61b6627
tate <tate@ens.domains>
2025-03-25 11:55:43 -0400

    header check

--- src/components/EnsipHeader.tsx
+++ src/components/EnsipHeader.tsx
@@ -4,18 +4,6 @@ type Props = {
   status: string
 }
 
-export function EnsipHeader({ authors, created, status }: Props) {
-  return (
-    <div className="flex flex-col gap-1">
-      <p>
-        <strong>Authors:</strong> {authors.join(', ')}
-      </p>
-      <p>
-        <strong>Created:</strong> {created}
-      </p>
-      <p>
-        <strong>Status:</strong> <span className="capitalize">{status}</span>
-      </p>
-    </div>
-  )
+export function EnsipHeader(_: Props) {
+  return null
 }
